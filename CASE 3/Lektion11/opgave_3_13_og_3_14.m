94e37d55484d3f24c08fe4d7d817c7ffeede8d10
mortenhaahr <mortenhaahrkristensen@gmail.com>
2019-11-20 16:14:18 +0100

    Exercises no longer gone

--- CASE 3/Lektion11/opgave_3_13_og_3_14.m
+++ CASE 3/Lektion11/opgave_3_13_og_3_14.m
@@ -34,22 +34,13 @@ clc
 % Opgave a løst i Mathcad.
 % nedenfor er opgave b og frem:
 
-% (s+w1)/(s+w2)
-
 w1 = 565;
 w2 = 942;
 Ts = 0.001;
 
-laplace = tf([1 w1], [1 w2]);
-
-z_transform = c2d(laplace,Ts)
-
-% Output c2d:
-% H(z) = (z - 0.634)/(z - 0.3898)
-
-%Bestemmelse af koefficienter fra output af c2d (skal ændres)
-b = [1,-0.634];
-a = [1,-0.3898];
+%Bestemmelse af koefficienter fra Mathcad
+b = [2.56,-1.43];
+a = [2.94,-1.06];
 
 %Vis frekvensresponsen for H(z) vha. freqz.m i Matlab
 % Tegn filter
@@ -58,13 +49,11 @@ freqz(b,a)
 title('Shelf filter frekvensrespons');
 
 %Er H(z) filteret stabilt? Hvorfor / hvorfor ikke?
-nulpunkter = roots([1,-0.634]);
-poler = roots([1,-0.3898]);
+nulpunkter = roots(b);
+poler = roots(a);
 
 figure(102);
 zplane(nulpunkter, poler);
 %Filtret er stabilt da alle poler er inden for enhedscirklen. Dvs. mellem
 %-1 og 1.
 
-%Find differensligningen der repræsenterer H(z), altså y(n) = ... 
-% Er det en invfft her??
